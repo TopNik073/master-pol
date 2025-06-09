34432b414300ce0c18a446a4f4a219f88cf656bd
TopNik073 <borzovn169@gmail.com>
2025-06-09 20:25:14 +0200

    fix(config)

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,15 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import PostgresDsn
 
 
 class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
     APP_NAME: str = "master-pol"
     APP_HOST: str = "localhost"
     APP_PORT: int = 8000
 
-    BD_HOST: str
+    DB_HOST: str
     DB_PORT: str
     DB_USER: str
     DB_PASS: str
