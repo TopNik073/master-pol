83e3728e8b4559b49e54fca5058c3048772c1370
TopNik073 <borzovn169@gmail.com>
2025-06-09 20:39:12 +0200

    Update app config

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
+from src.core.config import config
+
 from src.presentation.front import front_router
 
-app = FastAPI()
+app = FastAPI(title=config.APP_NAME)
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 app.include_router(front_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host=config.APP_HOST, port=config.APP_PORT)
